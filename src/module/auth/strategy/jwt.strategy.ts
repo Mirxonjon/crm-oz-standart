35e708b38346f701e8edbb69f04b5d5d238453c5
Mirxonjon <mirxonjonismanov152@gmail.com>
2024-11-21 18:39:52 +0500

    fixed bug

--- src/module/auth/strategy/jwt.strategy.ts
+++ src/module/auth/strategy/jwt.strategy.ts
@@ -3,12 +3,13 @@ import {
   HttpStatus,
   UnauthorizedException,
 } from '@nestjs/common';
+import * as dotenv from 'dotenv';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 // import { ControlUsersEntity } from 'src/entities/control_users.entity';
 import { UsersEntity } from 'src/entities/users.entity';
 import { CustomRequest } from 'src/types';
-
+dotenv.config();
 export class jwtStrategy extends PassportStrategy(Strategy) {
   constructor() {
     super({
